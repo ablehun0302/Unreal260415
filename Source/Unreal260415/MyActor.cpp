15126fa0b9df12bf2153276d5cfb157de319e607
Kim Younghun <ablehun@naver.com>
2026-04-15 14:34:56 +0900

    로켓 추가

---
+++ Source/Unreal260415/MyActor.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	RootComponent = Box;
+	Box->SetBoxExtent(FVector(29.50f, 8.0f, 8.0f));
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Rocket(TEXT("/Script/Engine.StaticMesh'/Game/Plane/Mesh/SM_Rocket.SM_Rocket'"));
+
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
+	Body->SetupAttachment(Box);
+	Body->SetRelativeRotation(FRotator(-90.0f, 0.0f, 0.0f));
+
+	if (SM_Rocket.Succeeded())
+	{
+		Body->SetStaticMesh(SM_Rocket.Object);
+	}
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovement->InitialSpeed = MoveSpeed;
+	ProjectileMovement->ProjectileGravityScale = 0.01f;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetLifeSpan(5.0f);
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
